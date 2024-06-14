29e07ec441ef5a1c831818d1b15f16b3d8679123
Dogcatlionz <161786003+Dogcatlionz@users.noreply.github.com>
2024-06-14 10:34:53 +0800

    Create 照片.js

---
+++ mid/照片.js
@@ -0,0 +1,30 @@
+const images = document.querySelectorAll('.slideshow img');
+let currentIndex = 0;
+
+function showImage(index) {
+    images.forEach((img, i) => {
+        img.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
+
+// 點擊照片左右切換
+images.forEach((img, i) => {
+    img.addEventListener('click', () => {
+        if (i === currentIndex) {
+            nextImage();
+        }
+    });
+});
+
+// 初始顯示第一張照片
+showImage(currentIndex);
