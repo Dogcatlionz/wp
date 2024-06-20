17b6f9301f55c5582a5a2f8b54d071f2dd4c1f19
Dogcatlionz <161786003+Dogcatlionz@users.noreply.github.com>
2024-06-20 23:14:55 +0800

    Create main.js

---
+++ 20240531/main.js
@@ -0,0 +1,87 @@
+import {sqlFetch} from '../lib/sql.js'// 引入 sqlFetch 函數
+
+export var R = {}
+let _id=0, _title=1, _body=2
+
+// 當網址的 hash 改變時，調用async function ()這個函式
+window.onhashchange = async function () {
+  var r
+  var tokens = window.location.hash.split('/')
+  console.log('tokens=', tokens)
+  switch (tokens[0]) {
+    case '#show':
+      // 如果是 '#show'，會做下列的事
+      let r = await sqlFetch('blog', `SELECT id, title, body FROM posts WHERE id=${tokens[1]}`)
+      R.show(r[0]) // 取得第一筆傳入 (雖然只會有一筆，但 SELECT 預設會傳回很多比，所以用 results[0] 只取第一筆)
+      break
+    case '#new':
+      // 如果是 '#new'，會調用R.new()
+      R.new()
+      break
+    default:
+      let posts = await sqlFetch('blog', `SELECT id, title, body FROM posts`)
+      R.list(posts)
+      break
+  }
+}
+
+// 當頁面加載完成時，調用async function ()這個函式
+window.onload = async function () {
+  await sqlFetch('blog', `CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, body TEXT)`)
+  window.onhashchange()
+}
+
+// 這個函式會更新title和content
+R.layout = function (title, content) {
+  document.querySelector('title').innerText = title
+  document.querySelector('#content').innerHTML = content
+}
+
+R.list = function (posts) {
+  let list = []
+  for (let post of posts) {
+    list.push(`
+    <li>
+      <h2>${post[_title]}</h2>
+      <p><a id="show${post[_id]}" href="#show/${post[_id]}">Read post</a></p>
+    </li>
+    `)
+  }
+  let content = `
+  <h1>Posts</h1>
+  <p>You have <strong>${posts.length}</strong> posts!</p>
+  <p><a id="createPost" href="#new">Create a Post</a></p>
+  <ul id="posts">
+    ${list.join('\n')}
+  </ul>
+  `
+  return R.layout('Posts', content)
+}
+
+//這個函式會建立新的貼文
+R.new = function () {
+  R.layout('New Post', `
+  <h1>New Post</h1>
+  <p>Create a new post.</p>
+  <form>
+    <p><input id="title" type="text" placeholder="Title" name="title"></p>
+    <p><textarea id="body" placeholder="Contents" name="body"></textarea></p>
+    <p><input id="savePost" type="button" value="Create"></p>
+  </form>
+  `)
+  document.querySelector('#savePost').onclick = ()=>R.savePost()
+}
+
+R.show = function (post) {
+  return R.layout(post[_title], `
+    <h1>${post[_title]}</h1>
+    <p>${post[_body]}</p>
+  `)
+}
+
+R.savePost = async function () {
+  let title = document.querySelector('#title').value
+  let body = document.querySelector('#body').value
+  await sqlFetch('blog', `INSERT INTO posts (title, body) VALUES ('${title}', '${body}')`)
+  window.location.hash = '#list'
+}
